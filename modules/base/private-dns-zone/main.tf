b2f5f54178d37cbd2221c036020f694aab0e063c
bigcat3997 <vqt.develop@gmail.com>
2025-12-31 22:01:12 +0700

    add new base module: private-dns-zone

---
+++ modules/base/private-dns-zone/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_private_dns_zone" "this" {
+  count               = var.create ? 1 : 0
+  resource_group_name = var.resource_group_name
+  name                = var.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "this" {
+  for_each = {
+    for idx, vnet_id in var.vnet_ids :
+    idx => vnet_id
+  }
+
+  resource_group_name   = var.resource_group_name
+  name                  = "dnslink-${substr(md5(each.value), 0, 8)}"
+  private_dns_zone_name = azurerm_private_dns_zone.this[0].name
+  virtual_network_id    = each.value
+
+  depends_on = [azurerm_private_dns_zone.this]
+}
---
+++ modules/base/private-dns-zone/variables.tf
@@ -0,0 +1,25 @@
+variable "create" {
+  description = "Create new one or use exist one"
+  type        = bool
+  default     = true
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group where resources will be created"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+}
+
+variable "name" {
+  description = "Base name used for Private DNS Zone and related resources"
+  type        = string
+}
+
+variable "vnet_ids" {
+  description = "List of VNet IDs to link to the Private DNS zone"
+  type        = list(string)
+}
